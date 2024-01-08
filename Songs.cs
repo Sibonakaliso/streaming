70afbef035dffea2069883945cd33dcb5cdb0715
Sibonakaliso <67508480+Sibonakaliso@users.noreply.github.com>
2024-01-08 12:37:54 +0200

    Add files via upload

---
+++ Songs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Stream2nd.Models
+{
+    public class Songs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SongID { get; set; }
+        [Required]
+        [Display(Name = "Artist")]
+        public string ArtistName { get; set; }
+        [Required]
+        [Display(Name = "Song Title")]
+        public string Title { get; set; }
+        
+        [Display(Name = "Upload Song")]
+        public string SongPath { get; set; }
+        
+
+    }
+}
---
+++ SongsDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Stream2nd.Models
+{
+    public class SongsDBContext : DbContext
+    {
+        public SongsDBContext() : base("SongsDBContext")
+        {
+
+        }
+        public DbSet<Songs> Songs { get; set; }
+    }
+}
